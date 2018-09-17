7fa415e30b4b1812db243d809ba3fe1a4f3a4cf9
Dawit Thepchatree <dawit@Payoon-Air.local>
2018-09-17 14:02:52 +0700

    Revert Change

--- HelloWorld.py
+++ HelloWorld.py
@@ -2,6 +2,11 @@ print("Hello World!")
 print("Hello Git!")
 print("Testing New Edit After Commit!")
 
+print("This is a change by another person!")
+
+print("Hmm, this is an interesting change!")
+print("Made a change there!")
+
 print("Change A")
 print("Change B")
 print("Change C")
