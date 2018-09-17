698034b22ea6602f99bac899abe5090d6feda95f
DawitJoe <43329570+DawitJoe@users.noreply.github.com>
2018-09-17 13:47:09 +0700

    Update HelloWorld.py
    This is something that needs to be changed!

--- HelloWorld.py
+++ HelloWorld.py
@@ -3,3 +3,7 @@ print("Hello Git!")
 print("Testing New Edit After Commit!")
 
 print("This is a change by another person!")
+
+
+print("Hmm, this is an interesting change!")
+print("Made a change there!")
