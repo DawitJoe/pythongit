ddd12fedcb571a7776c6cadb870c57f9042de1b6
Dawit Thepchatree <dawit@Payoon-Air.local>
2018-09-17 13:59:26 +0700

    Fixed

--- HelloWorld.py
+++ HelloWorld.py
@@ -2,18 +2,11 @@ print("Hello World!")
 print("Hello Git!")
 print("Testing New Edit After Commit!")
 
-<<<<<<< HEAD
 print("This is a change by another person!")
-<<<<<<< HEAD
-print("Made a change here.")
-=======
-
 
 print("Hmm, this is an interesting change!")
 print("Made a change there!")
->>>>>>> 698034b22ea6602f99bac899abe5090d6feda95f
-=======
+
 print("Change A")
 print("Change B")
 print("Change C")
->>>>>>> 300d5d227e75774ac800e1f034bcc3d20be3932f
