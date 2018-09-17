22d064f586edcf042b8a2ed4684f887492e1828c
Dawit Thepchatree <dawit@Payoon-Air.local>
2018-09-17 14:04:50 +0700

    version 4

--- HelloWorld.py
+++ HelloWorld.py
@@ -1,12 +1,12 @@
 print("Hello World!")
 print("Hello Git!")
-print("Testing New Edit After Commit!")
 
-print("This is a change by another person!")
 
-print("Hmm, this is an interesting change!")
-print("Made a change there!")
+print("I made another change.")
 
 print("Change A")
 print("Change B")
-print("Change C")
+
+
+
+print("Now I am making another change!")
